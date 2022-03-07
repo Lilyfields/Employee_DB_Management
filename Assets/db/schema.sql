e21b306e16f16d8ae1a59c2525ad4dd0f37ed59f
Lilyfields <lilyfields11@bigpond.com>
2022-03-07 15:18:54 +1100

    removed route and model,as not required

--- Assets/db/schema.sql
+++ Assets/db/schema.sql
@@ -4,25 +4,26 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL,
+  name VARCHAR(30)
 
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
-  PRIMARY KEY (id)
+  FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id VARCHAR(30),
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
   manager_id INT,
-  PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
